a0ff1d12fcb70862ad0451466ee1a538cf1f8724
Aditya Diwakar <aditya@diwakar.io>
2023-10-27 12:31:16 -0400

    add directive parsing and raw strings

--- lex.py
+++ lex.py
@@ -12,6 +12,7 @@ INT_WORD    = "INT_WORD"
 FLOAT_WORD  = "FLOAT_WORD"
 INT         = "INT"
 FLOAT       = "FLOAT"
+STR         = "STR"
 IDENT       = "IDENT"
 WHILE       = "WHILE"
 NEQ         = "NEQ"
@@ -34,15 +35,17 @@ NOT         = "NOT"
 COMMA       = "COMMA"
 GT          = "GT"
 LT          = "LT"
+DIRECTIVE   = "DIRECTIVE"
+ANDREF      = "ANDREF"
 
 LANG_NUMBERS = list(map(str, range(0, 10)))
 LANG_NUMBERS_FLOAT = LANG_NUMBERS + ["."]
 
-LANG_FIRST_LETTER_IDENT = [chr(a) for a in list(range(65, 91)) + list(range(97, 123))]
+LANG_FIRST_LETTER_IDENT = [chr(a) for a in list(range(65, 91)) + list(range(97, 123))] + ["#"]
 LANG_IDENT = LANG_FIRST_LETTER_IDENT + ["_"] + LANG_NUMBERS
 LANG_RESERVED = ["int", "float", "while", "if", "return"]
 LANG_PAIRS = ["(", ")", "{", "}"]
-LANG_OPERATORS = ["!", "-", "+", "=", "/", "*", ">", "<"]
+LANG_OPERATORS = ["&", "!", "-", "+", "=", "/", "*", ">", "<"]
 
 class Token():
     def __init__(self, t_type, value=None):
@@ -59,56 +62,75 @@ buffer = ""
 tokens = []
 ident_flag = False 
 numerical_flag = False
+str_flag = False
 for character in gen:
-    if not ident_flag and character in LANG_FIRST_LETTER_IDENT:
-        ident_flag = True
-        buffer += character
-    elif ident_flag and character in LANG_IDENT:
-        buffer += character
-    elif ident_flag:
-        if buffer not in LANG_RESERVED:
-            tokens.append(Token(IDENT, "".join(buffer)))
-        else:
-            if buffer == "int": tokens.append(Token(INT_WORD))
-            elif buffer == "float": tokens.append(Token(FLOAT_WORD))
-            elif buffer == "while": tokens.append(Token(WHILE))
-            elif buffer == "if": tokens.append(Token(IF))
-            elif buffer == "return": tokens.append(Token(RETURN))
-        ident_flag = False
-        buffer = ""
-
-    if not numerical_flag and character in LANG_NUMBERS and not ident_flag:
-        buffer += character
-        numerical_flag = True
-    elif numerical_flag:
-        if character in LANG_NUMBERS_FLOAT:
+    # start string consuming procedure
+    if not str_flag and character == '"':
+        str_flag = True
+    elif str_flag:
+        if character != '"':
             buffer += character
         else:
-            if "." in buffer: tokens.append(Token(FLOAT, buffer))
-            else: tokens.append(Token(INT, buffer))
-            numerical_flag = False
+            str_flag = False
+            tokens.append(Token(STR, buffer))
             buffer = ""
 
+    if not str_flag:
+        if not ident_flag and character in LANG_FIRST_LETTER_IDENT:
+            ident_flag = True
+            buffer += character
+        elif ident_flag and character in LANG_IDENT:
+            buffer += character
+        elif ident_flag:
+            if "#" in buffer:
+                tokens.append(Token(DIRECTIVE, buffer[1:]))
+            elif buffer not in LANG_RESERVED:
+                tokens.append(Token(IDENT, buffer))
+            else:
+                if buffer == "int": tokens.append(Token(INT_WORD))
+                elif buffer == "float": tokens.append(Token(FLOAT_WORD))
+                elif buffer == "while": tokens.append(Token(WHILE))
+                elif buffer == "if": tokens.append(Token(IF))
+                elif buffer == "return": tokens.append(Token(RETURN))
+            ident_flag = False
+            buffer = ""
 
-    if character == ",": tokens.append(Token(COMMA))
+    if not str_flag and not ident_flag:
+        if not numerical_flag and character in LANG_NUMBERS:
+            buffer += character
+            numerical_flag = True
+        elif numerical_flag:
+            if character in LANG_NUMBERS_FLOAT:
+                buffer += character
+            else:
+                if "." in buffer: tokens.append(Token(FLOAT, buffer))
+                else: tokens.append(Token(INT, buffer))
+                numerical_flag = False
+                buffer = ""
 
-    if character in LANG_OPERATORS:
-        if character == "+": tokens.append(Token(PLUS))
-        if character == "-": tokens.append(Token(SUB))
-        if character == "*": tokens.append(Token(MULT))
-        if character == "/": tokens.append(Token(DIV))
-        if character == "!": tokens.append(Token(NOT))
-        if character == "=": tokens.append(Token(EQUALS))
-        if character == ">": tokens.append(Token(GT))
-        if character == "<": tokens.append(Token(LT))
 
-    if character in LANG_PAIRS: 
-        if character == "(": tokens.append(Token(LPAREN))
-        if character == ")": tokens.append(Token(RPAREN))
-        if character == "{": tokens.append(Token(LBRACK))
-        if character == "}": tokens.append(Token(RBRACK))
-        ident_flag = False
-        buffer = ""
+    if not str_flag:
+        if character == ";": tokens.append(Token(SEMI))
+        if character == ",": tokens.append(Token(COMMA))
+
+        if character in LANG_OPERATORS:
+            if character == "+": tokens.append(Token(PLUS))
+            if character == "-": tokens.append(Token(SUB))
+            if character == "*": tokens.append(Token(MULT))
+            if character == "/": tokens.append(Token(DIV))
+            if character == "!": tokens.append(Token(NOT))
+            if character == "=": tokens.append(Token(EQUALS))
+            if character == ">": tokens.append(Token(GT))
+            if character == "<": tokens.append(Token(LT))
+            if character == "&": tokens.append(Token(ANDREF))
+
+        if character in LANG_PAIRS: 
+            if character == "(": tokens.append(Token(LPAREN))
+            if character == ")": tokens.append(Token(RPAREN))
+            if character == "{": tokens.append(Token(LBRACK))
+            if character == "}": tokens.append(Token(RBRACK))
+            ident_flag = False
+            buffer = ""
 
 
 for token in tokens: print(token)
