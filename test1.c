ebfd776f8458f37708ec052d5f524a82d63575ce
CC-734 <clarecotter7@gmail.com>
2023-11-03 15:20:31 -0400

    test case 1

--- test1.c
+++ test1.c
@@ -1,15 +1,70 @@
 #include "stdio.h"
 
 int main() {
-    int x ;
+    int x;
     int y;
     int max;
-    scanf("%i %i",&x,&y);
+    scanf("%d %d",&x,&y);
     if (x > y) {
         max = x;
-    } else if (x < y) {
-        max = y;
     } else {
-        printf("Numbers are equal");
+        max = y;
     }
+    printf("%d",max);
 }
+
+/*
+DIRECTIVE
+STR
+INT_WORD
+IDENT(main)
+LBRACK
+
+INT_WORD
+IDENT(x)
+SEMI
+INT_WORD
+IDENT(y)
+SEMI
+INT_WORD
+IDENT(max)
+SEMI
+
+IDENT(scanf)
+LPAREN
+STR
+COMMA
+IDENT(x)
+COMMA
+IDENT(y)
+RPAREN
+SEMI
+
+IF
+LPAREN
+IDENT(x)
+GT
+IDENT(y)
+RBRACK
+IDENT(max)
+EQUALS
+IDENT(x)
+SEMI
+RBRACK
+
+ELSE
+LBRACK
+IDENT(max)
+EQUALS
+IDENT(y)
+SEMI
+RBRACK
+
+IDENT(printf)
+STR
+COMMA
+IDENT(max)
+SEMI
+
+RBRACK
+*/
