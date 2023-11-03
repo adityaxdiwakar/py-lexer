539d00b90cba6c96f073d684159dbf6c4a3a9a3a
CC-734 <clarecotter7@gmail.com>
2023-11-03 15:31:02 -0400

    second test case

---
+++ tests/test2.c
@@ -0,0 +1,36 @@
+#include "stdio.h"
+
+int main() {
+    float a = 10.0;
+    float b = 4.0;
+    return a / b;
+}
+
+/*
+DIRECTIVE
+STR
+INT_WORD
+IDENT(main)
+LBRACK
+
+FLOAT_WORD
+IDENT(a)
+EQUALS
+FLOAT
+SEMI
+
+FLOAT_WORD
+IDENT(b)
+EQUALS
+FLOAT
+SEMI
+
+RETURN
+IDENT(a)
+DIV
+IDENT(b)
+SEMI
+
+RBRACK
+
+*/
