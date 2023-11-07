dea3c4d21708d44ee32a23bae1541e54d133cb08
CC-734 <clarecotter7@gmail.com>
2023-11-06 22:54:29 -0500

    fixing old tests and adding test4

--- tests/test2.c
+++ tests/test2.c
@@ -5,28 +5,3 @@ int main() {
     float b = 4.0;
     return a / b;
 }
-
-/*
-DIRECTIVE
-STR(stdio.h)
-INT_WORD
-IDENT(main)
-LBRACK
-
-FLOAT_WORD
-IDENT(a)
-EQUALS
-FLOAT(10.0)
-SEMI
-FLOAT_WORD
-IDENT(b)
-EQUALS
-FLOAT(4.0)
-SEMI
-RETURN
-IDENT(a)
-DIV
-IDENT(b)
-SEMI
-RBRACK
-*/
