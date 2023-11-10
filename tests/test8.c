acc0b5b7d9fe26139350e09cc8463aac5dd9dbba
ritvik-knagaram <ritvik.knagaram@gatech.edu>
2023-11-10 15:44:25 -0500

    Test Case 8

---
+++ tests/test8.c
@@ -0,0 +1,9 @@
+void greet() {
+    printf("Hello, World!\n");
+}
+
+int main() {
+    greet();
+    
+    return 0;
+}
---
+++ tests/test9.c
@@ -0,0 +1,11 @@
+int main() {
+    int number = 10;
+
+    if (number > 5) {
+        printf("The number is greater than 5.\n");
+    } else {
+        printf("The number is not greater than 5.\n");
+    }
+
+    return 0;
+}
